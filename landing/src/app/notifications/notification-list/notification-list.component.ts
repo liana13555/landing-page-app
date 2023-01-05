99402809829f5411835786e5e28d34b584a84e62
liana13555 <barishovaliana@gmail.com>
2023-01-05 12:39:14 +0100

    Added notifications

--- landing/src/app/notifications/notification-list/notification-list.component.ts
+++ landing/src/app/notifications/notification-list/notification-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs";
+import { NotificationsService, Command } from "../notifications.service";
 
 @Component({
   selector: 'app-notification-list',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notification-list.component.css']
 })
 export class NotificationListComponent implements OnInit {
+  messages!: Observable<Command[]>
 
-  constructor() { }
+  constructor(private notificationsService: NotificationsService) {
+    this.messages = this.notificationsService.messagesOutput
+
+    // setInterval(() => {
+    //   this.notificationsService.addSuccess('IT IS errorly WORKING!!!')
+    // }, 2000)
+  }
 
   ngOnInit(): void {
+  }
+
+  clearMessage(id: number) {
+    this.notificationsService.clearMessage(id)
   }
 
 }
--- landing/src/app/notifications/notifications.service.ts
+++ landing/src/app/notifications/notifications.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Subject, Observable, ReplaySubject } from "rxjs";
 import { scan } from "rxjs/operators";
 
-interface Command {
+export interface Command {     // Command - message
   id: number
   type: 'success' | 'error' | 'clear'
   text?: string
@@ -29,19 +29,31 @@ export class NotificationsService {
   }
 
   addSuccess(message: string) {
+    const id = this.randomId()
+
     this.messagesInput.next({
-      id: this.randomId()
+      id,
       text: message,
       type: 'success'
     })
+
+    setTimeout(() => {
+      this.clearMessage(id)
+    }, 5000)
   }
 
   addError(message: string) {
+    const id = this.randomId()
+
     this.messagesInput.next({
-      id: this.randomId()
+      id,
       text: message,
       type: 'error'
     })
+    
+    setTimeout(() => {
+      this.clearMessage(id)
+    }, 5000)
   }
 
   clearMessage(id: number) {
