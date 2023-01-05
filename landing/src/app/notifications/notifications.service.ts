3933563ebe0e33e5baeaa8ead3bc0d149a8c54cb
liana13555 <barishovaliana@gmail.com>
2023-01-05 09:27:32 +0100

    Updated Notifications Service

--- landing/src/app/notifications/notifications.service.ts
+++ landing/src/app/notifications/notifications.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import { Subject, Observable, ReplaySubject } from "rxjs";
+import { scan } from "rxjs/operators";
+
+interface Command {
+  id: number
+  type: 'success' | 'error' | 'clear'
+  text?: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
+  messagesInput: Subject<Command>
+  messagesOutput: Observable<Command[]>
 
-  constructor() { }
+  constructor() {
+    this.messagesInput = new Subject<Command>()
+    this.messagesOutput = this.messagesInput.pipe(
+      scan((acc: Command[], value: Command) => {   // scan((messages, command) => {},[])
+        if (value.type === 'clear') {
+          return acc.filter(message => message.id !== value.id )
+        } else {
+          return [...acc, value]
+        }  
+      },[])
+    )
+  }
+
+  addSuccess(message: string) {
+    this.messagesInput.next({
+      id: this.randomId()
+      text: message,
+      type: 'success'
+    })
+  }
+
+  addError(message: string) {
+    this.messagesInput.next({
+      id: this.randomId()
+      text: message,
+      type: 'error'
+    })
+  }
+
+  clearMessage(id: number) {
+    this.messagesInput.next({
+      id,
+      type: 'clear'
+    })
+  }
+
+  private randomId() {
+    return Math.round(Math.random()*10000)
+  }
 }
